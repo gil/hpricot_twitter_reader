1c9f076dc17bf57d8804e006b1782c68b6654031
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-01-12 18:25:55 -0200

    twitter reader rr71

---
+++ twitter.rb
@@ -0,0 +1,19 @@
+require 'hpricot'
+require 'open-uri'
+
+def hpricot(account = "sr_valeriano")
+  file = open("http://twitter.com/#{account}")
+  Hpricot(file)
+end
+
+def tweets(doc)
+  items = doc / ".stream .stream-items .stream-item"
+  items.each do |item|
+    tweet = item / ".tweet .js-tweet-text"
+    puts tweet.inner_text
+    puts "_____________\n\n"
+  end
+end
+
+doc = hpricot("sr_valeriano")
+tweets(doc)
